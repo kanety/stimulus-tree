0071327268a8c1b2795932a31f16f878bd203bb5
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-10-10 11:51:04 +0900

    Refactor keyboard event spec

--- test/tree/index-spec.js
+++ test/tree/index-spec.js
@@ -15,18 +15,6 @@ describe('index', () => {
             <li data-node-id="1.1">
               <a href="#icon"></a>
               <span>1.1</span>
-              <ul>
-                <li data-node-id="1.1.1">
-                  <a href="#icon"></a>
-                  <span>1.1.1</span>
-                  <ul>
-                    <li data-node-id="1.1.1.1">
-                      <a href="#icon"></a>
-                      <span>1.1.1.1</span>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
             </li>
           </ul>
         </li>
@@ -39,43 +27,5 @@ describe('index', () => {
     expect($('[data-node-id="1"]').matches('.st-tree__node--closed')).toEqual(true);
     $('[data-node-id="1"] > a').click();
     expect($('[data-node-id="1"]').matches('.st-tree__node--closed')).toEqual(false);
-  });
-
-  describe('keyboard', () => {
-    it('moves focus by up key', () => {
-      $('[data-node-id="1.1"] > a').focus();
-      $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 38, bubbles: true }));
-      expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1');
-    });
-
-    it('moves focus by down key', () => {
-      $('[data-node-id="1.1"] > a').focus();
-      $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 40, bubbles: true }));
-      expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1.1.1');
-    });
-
-    it('moves focus by left key', () => {
-      $('[data-node-id="1.1"] > a').click();
-      $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37, bubbles: true }));
-      expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1');
-    });
-
-    it('closes node by left key', () => {
-      $('[data-node-id="1.1"] > a').focus();
-      $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37, bubbles: true }));
-      expect($('[data-node-id="1.1"]').matches('.st-tree__node--closed')).toEqual(true);
-    });
-
-    it('moves focus by right key', () => {
-      $('[data-node-id="1.1"] > a').focus();
-      $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39, bubbles: true }));
-      expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1.1.1');
-    });
-
-    it('opens node by right key', () => {
-      $('[data-node-id="1.1"] > a').click();
-      $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39, bubbles: true }));
-      expect($('[data-node-id="1.1"]').matches('.st-tree__node--closed')).toEqual(false);
-    });
   });
 });
---
+++ test/tree/keyboard-spec.js
@@ -0,0 +1,72 @@
+import { Application } from '@hotwired/stimulus';
+import TreeController from 'index';
+
+const application = Application.start();
+application.register('tree', TreeController);
+
+describe('index', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul data-controller="tree">
+        <li data-node-id="1">
+          <a href="#icon"></a>
+          <span>1</span>
+          <ul>
+            <li data-node-id="1.1">
+              <a href="#icon"></a>
+              <span>1.1</span>
+              <ul>
+                <li data-node-id="1.1.1">
+                  <a href="#icon"></a>
+                  <span>1.1.1</span>
+                  <ul>
+                    <li data-node-id="1.1.1.1">
+                      <a href="#icon"></a>
+                      <span>1.1.1.1</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    `;
+  });
+
+  it('moves focus by up key', () => {
+    $('[data-node-id="1.1"] > a').focus();
+    $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 38, bubbles: true }));
+    expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1');
+  });
+
+  it('moves focus by down key', () => {
+    $('[data-node-id="1.1"] > a').focus();
+    $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 40, bubbles: true }));
+    expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1.1.1');
+  });
+
+  it('moves focus by left key', () => {
+    $('[data-node-id="1.1"] > a').click();
+    $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37, bubbles: true }));
+    expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1');
+  });
+
+  it('closes node by left key', () => {
+    $('[data-node-id="1.1"] > a').focus();
+    $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37, bubbles: true }));
+    expect($('[data-node-id="1.1"]').matches('.st-tree__node--closed')).toEqual(true);
+  });
+
+  it('moves focus by right key', () => {
+    $('[data-node-id="1.1"] > a').focus();
+    $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39, bubbles: true }));
+    expect(document.activeElement.parentNode.getAttribute('data-node-id')).toEqual('1.1.1');
+  });
+
+  it('opens node by right key', () => {
+    $('[data-node-id="1.1"] > a').click();
+    $('[data-node-id="1.1"] > a').dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39, bubbles: true }));
+    expect($('[data-node-id="1.1"]').matches('.st-tree__node--closed')).toEqual(false);
+  });
+});
