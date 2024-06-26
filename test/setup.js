b58c37166ef532cb84bc77b3c76dd4ef40df1662
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-26 11:01:29 +0900

    Fix test and examples

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,10 @@
-global.$ = document.querySelector.bind(document);
-global.$$ = document.querySelectorAll.bind(document);
-
 import { Application } from '@hotwired/stimulus';
 import TreeController from 'index';
 
 global.application = Application.start();
 global.application.register('tree', TreeController);
+
+global.$ = document.querySelector.bind(document);
+global.$$ = document.querySelectorAll.bind(document);
 
 require('jest-fetch-mock').enableMocks();
